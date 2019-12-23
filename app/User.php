6c539c869b07f806e40120f361f4ae75816b3521
ChamalJaliya <cjaliya.sln2@gmail.com>
2019-12-24 00:57:03 +0530

    generate question model via artisan model

--- app/User.php
+++ app/User.php
@@ -28,6 +28,15 @@ class User extends Authenticatable
         'password', 'remember_token',
     ];
 
+    public function questions(){
+        return $this->hasMany(Question::class);
+    }
+    public function SetTitleAttribute($value)
+    {
+        $this->attributes['title'] = $value;
+        $this->attributes['slug'] = str_slug($value);
+    }
+
     /**
      * The attributes that should be cast to native types.
      *
