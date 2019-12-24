a17967646356f5d31179308e7670af9752148af4
ChamalJaliya <cjaliya.sln2@gmail.com>
2019-12-25 01:30:32 +0530

    Auth Policies

--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -36,14 +36,18 @@
                                 <div class="d-flex align-items-center">
                                     <h3 class="mt-0"><a href="{{$question->url}}">{{$question->title}}</a></h3>
                                 <div class="ml-auto">
+                                    @can ('update',$question)
                                     <a href="{{route('questions.edit',$question->id)}}" class="btn btn-sm btn-outline-info"> Edit</a>
-                                <form class="form-delete" action="{{ route('questions.destroy',$question->id)}}" method="post">
+                                @endcan
+                                @can ('delete',$question)
+                                    <form class="form-delete" action="{{ route('questions.destroy',$question->id)}}" method="post">
                                 @method('DELETE')
                                 @csrf
                                 <button type="submit" class="btn btn-sm btn-outline-danger" onclick="return confirm('Are You Sure')">
                                     Delete
                                 </button>
                                 </form>
+                                @endcan
                                 </div>
                                 </div>
                            
