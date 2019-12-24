a0d06378f9d1c74db6067527070c0267dfbac3aa
ChamalJaliya <cjaliya.sln2@gmail.com>
2019-12-24 23:33:02 +0530

    extend the form functionality update question

--- app/Http/Controllers/QuestionsController.php
+++ app/Http/Controllers/QuestionsController.php
@@ -69,7 +69,8 @@ class QuestionsController extends Controller
      */
     public function edit(Question $question)
     {
-        //
+        // $question = Question::findOrFail($id);
+        return view('questions.edit' ,compact('question'));
     }
 
     /**
@@ -79,9 +80,11 @@ class QuestionsController extends Controller
      * @param  \App\Question  $question
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Question $question)
+    public function update(AskQuestionRequest $request, Question $question)
     {
-        //
+        $question->update($request->only('title','body'));
+
+        return redirect('/questions')->with('success', 'Your question has been updated');
     }
 
     /**
