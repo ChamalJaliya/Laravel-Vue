39b097c55c66b35bbd32ea8ccfd02c8b338a72ea
ChamalJaliya <cjaliya.sln2@gmail.com>
2019-12-26 01:29:02 +0530

    design answer scheme and fake Answer Fact

--- app/Question.php
+++ app/Question.php
@@ -26,7 +26,7 @@ class Question extends Model
     }
     public function getStatusAttribute()
     {
-        if($this->answers > 0){
+        if($this->answers_count > 0){
             if($this->best_answer_id)
             {
                 return "answered-accepted";
@@ -41,5 +41,10 @@ class Question extends Model
         {
             return \Parsedown::instance()->text($this->body);
         }
-   
+   //Relationship Methods to Answer Model
+   public function answers()
+   {
+       return $this->hasMany(Answer::class);
+
+   }
 }
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,14 +11,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+      
         factory(App\User::class,3)->create()->each(function($u){
             $u->questions()
             ->saveMany(
-                factory(App\Question::class,rand (1,5))
-                ->make()
-            );
+                factory(App\Question::class,rand (1, 5))->make()
+            )
+            ->each(function ($q) {
+                $q->answers()->saveMany(factory(App\Answer::class ,rand(1,5))->make());
+            });
         });
-        
     }
 }
