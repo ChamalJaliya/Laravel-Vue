d9f4998d1e6c993d896dfc11c69f36d67a87805f
ChamalJaliya <cjaliya.sln2@gmail.com>
2019-12-31 01:17:05 +0530

    Favourite

--- app/Question.php
+++ app/Question.php
@@ -1,55 +1,65 @@
 <?php
-
 namespace App;
-
 use Illuminate\Database\Eloquent\Model;
-
 class Question extends Model
 {
-    //
-    protected $fillable = ['title' , 'body'];
-    public function user(){
+    protected $fillable = ['title', 'body'];
+    
+    public function user() {
         return $this->belongsTo(User::class);
-    }
-
-    public function SetTitleAttribute($value)
+    }    
+    public function setTitleAttribute($value)
     {
         $this->attributes['title'] = $value;
         $this->attributes['slug'] = str_slug($value);
     }
-    //create a accessor
-    public function getUrlAttribute(){
-        return route('questions.show',$this->slug);
+    public function getUrlAttribute()
+    {
+        return route("questions.show", $this->slug);
     }
-    public function getCreatedDateAttribute(){
+    public function getCreatedDateAttribute()
+    {
         return $this->created_at->diffForHumans();
     }
     public function getStatusAttribute()
     {
-        if($this->answers_count > 0){
-            if($this->best_answer_id)
-            {
+        if ($this->answers_count > 0) {
+            if ($this->best_answer_id) {
                 return "answered-accepted";
             }
-             return "answered";
-            
+            return "answered";
         }
-        return "unaswered";
+        return "unanswered";
     }
-
-        public function getBodyHtmlAttribute()
-        {
-            return \Parsedown::instance()->text($this->body);
-        }
-   //Relationship Methods to Answer Model
-   public function answers()
-   {
-       return $this->hasMany(Answer::class);
-
-   }
-   public function acceptBestAnswer(Answer $answer)
-   {
-       $this->best_answer_id = $answer->id;
-       $this->save();
+    public function getBodyHtmlAttribute()
+    {
+        return \Parsedown::instance()->text($this->body);
+    }
+    public function answers()
+    {
+        return $this->hasMany(Answer::class);
+        // $question->answers->count()
+        // foreach ($question->answers as $answer)
+    }
+    public function acceptBestAnswer(Answer $answer)
+    {
+        $this->best_answer_id = $answer->id;
+        $this->save();
+    }
+    public function favorites()
+    {
+        return $this->belongsToMany(User::class, 'favorites')->withTimestamps(); //, 'question_id', 'user_id');
+    }
+    public function isFavorited()
+    {
+        return $this->favorites()->where('user_id', auth()->id())->count() > 0;
+    }
+    public function getIsFavoritedAttribute()
+    {
+        return $this->isFavorited();
+    }
+    public function getFavoritesCountAttribute()
+    {
+        return $this->favorites->count();
     }
 }
