5c83a6a762d73b96d791f8bb82d3cb3516bc3e12
ChamalJaliya <cjaliya.sln2@gmail.com>
2019-12-30 09:41:28 +0530

    Delete functionality for answer

--- app/Answer.php
+++ app/Answer.php
@@ -6,29 +6,38 @@ use Illuminate\Database\Eloquent\Model;
 
 class Answer extends Model
 {
-    protected $fillable =['body','user_id'];
+    protected $fillable = ['body', 'user_id'];
+    
     public function question()
     {
-        return $this->belongsTo(Question::class); 
-
+        return $this->belongsTo(Question::class);
     }
+
     public function user()
     {
-        return $this->belongsTo(User::class); 
+        return $this->belongsTo(User::class);
     }
 
     public function getBodyHtmlAttribute()
-        {
-            return \Parsedown::instance()->text($this->body);
-        }
-        public  static function boot()
-        {
-            parent::boot();
-            static::created(function($answer){
-                $answer->question->increment('answers_count');
-            });
-        }
-        public function getCreatedDateAttribute(){
-            return $this->created_at->diffForHumans();
-        }
+    {
+        return \Parsedown::instance()->text($this->body);
+    }
+    
+    public static function boot()
+    {
+        parent::boot();
+
+        static::created(function ($answer) {
+            $answer->question->increment('answers_count');                     
+        });        
+
+        static::deleted(function ($answer) {
+            $answer->question->decrement('answers_count');
+        });
+    }
+
+    public function getCreatedDateAttribute()
+    {
+        return $this->created_at->diffForHumans();
+    }
 }
